1c29a9a48a08d6df738d1df093456701689be44a
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-02-18 10:09:22 +0100

    Van Hazelaar: verbetering van toelichting in sketch en README.

--- O2SensorTester/O2SensorTester.ino
+++ O2SensorTester/O2SensorTester.ino
@@ -1,15 +1,15 @@
 /************
 * Deze sketch wordt opgezet om de O2-sensor
-* van de Morris te onderzoeken, los van Speeduino.
-* De sensor, l2 van Innovate, wordt aangesloten via de controler:
+* van onze Morris te onderzoeken, los van Speeduino.
+* De sensor, LC-2 van Innovate, wordt aangesloten via de controller:
 * zwart =  ground
 * rood = 12V, tevens de voeding van de Arduino op Vin:
-* bruin = output 0 - 5 volt naar A0;
-* geel = output (nog niet gebruikt)
-* Plan: zelfstandig instrument maken met eigen display.
+* geel = output 0 (=7.35AFR)- 5 (22.39AFR) volt naar A0;
+* bruin = output 1.1 (=14AFR) en 0.1 (=15AFR) volt (nog niet gebruikt)
+* Doel: zelfstandig instrument maken met eigen display.
 * 
 *************/
-
+int O2;
 
 void setup() {
   Serial.begin(115200);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MotorArduino
 Software for the motor management system of our Morris motor
+. Doel is een zelfstandige O2-meter te maken met eigen display
