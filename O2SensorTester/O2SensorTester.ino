f4b8254838ab3665503b95da1f07d0f57ec5d65d
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-02-18 16:03:23 +0100

    jan@hazelaar: voegt formule toe om resultaat analogRead() om te rekenen naar AFR; getest met spiritusdoek tegen de sensor: AFR 7.35; getest met spiritusdoek losjes op de sensor gelegd: verloopt langzaam van AFR 8 naar AFR 23.39 met stapjes in de orde van 0.1 AFR.

--- O2SensorTester/O2SensorTester.ino
+++ O2SensorTester/O2SensorTester.ino
@@ -4,21 +4,22 @@
 * De sensor, LC-2 van Innovate, wordt aangesloten via de controller:
 * zwart =  ground
 * rood = 12V, tevens de voeding van de Arduino op Vin:
-* geel = output 0 (=7.35AFR)- 5 (22.39AFR) volt naar A0;
-* bruin = output 1.1 (=14AFR) en 0.1 (=15AFR) volt (nog niet gebruikt)
+* geel = output1 0 (=7.35AFR)- 5 (22.39AFR) volt naar A0;
+* bruin = output2 1.1 (=14AFR) en 0.1 (=15AFR) volt (nog niet gebruikt)
 * Doel: zelfstandig instrument maken met eigen display.
 * 
 *************/
-int O2;
+float O2;
+float factor;
 
 void setup() {
   Serial.begin(115200);
-
+  factor = (23.39 - 7.35)/1023;
 }
 
 void loop() {
 O2 = analogRead(A0);
+O2 = 7.35 + O2 * factor;
 Serial.println(O2);
 delay(1000);
-
 }
