5ae8c6a2ad7c8b79f346955aee6caa1dde366334
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-02-23 10:18:30 +0100

    jan@hazelaar: test uitgevoerd met sensor en dat vermeld in het commentaar in de sketch

--- O2SensorTester/O2SensorTester.ino
+++ O2SensorTester/O2SensorTester.ino
@@ -7,7 +7,9 @@
 * geel = output1 0 (=7.35AFR)- 5 (22.39AFR) volt naar A0;
 * bruin = output2 1.1 (=14AFR) en 0.1 (=15AFR) volt (nog niet gebruikt)
 * Doel: zelfstandig instrument maken met eigen display.
-* 
+* Deze versie getest op 23-02-2024 met spiritusdoek op de sensor en 
+* daarmee het verdampingsverloop gevolgd mbv serial plotter. Dat 
+* duurde wel een minuut of 12.
 *************/
 float O2;
 float factor;
